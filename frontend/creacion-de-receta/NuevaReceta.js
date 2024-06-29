6f2b11885609c2494376733a3a58d3d932e718cc
Gonzaa <gweber@fi.uba.ar>
2024-06-28 23:38:54 -0300

    Se agrego y corrigieron variables

--- frontend/creacion-de-receta/NuevaReceta.js
+++ frontend/creacion-de-receta/NuevaReceta.js
@@ -7,23 +7,29 @@ function handle_response(data){
 }
 
 function create_recipe(event){
+    const parametros_URL = new URLSearchParams(window.location.search)
+    const id_usuario = parametros_URL.get("id_usuario")
+    if (id_usuario === null) {
+        window.location.href = `/`
+    }
     event.preventDefault()
     const formData = new FormData(event.target)
-
     const nombre = formData.get("nombre")
     const descripcion = formData.get("descripcion")
     const imagen = formData.get("imagen")
     const video = formData.get("video")
     const ingredientes = formData.get("ingredientes")
     const receta = formData.get("receta")
+    const tipo_receta = formData.get("tipo_receta")
+    const dificultad = formData.get("dificultad")
     const calorias = formData.get("calorias")
     const tiempo_preparacion = formData.get("tiempo_preparacion")
-    
-    const alto_proteinas = document.getElementById('highProtein').checked;
-    const bajo_carbohidratos = document.getElementById('lowCarbs').checked;
-    const apto_vegano = document.getElementById('veganFriendly').checked;
-    const apto_celiaco = document.getElementById('celiacFriendly').checked;
-    const apto_lactosa = document.getElementById('lactoseFree').checked;
+    const alto_proteinas = formData.get("highProtein") ? true : false
+    const bajo_carbohidratos = formData.get("lowCarbs") ? true : false
+    const apto_vegano = formData.get("veganFriendly") ? true : false
+    const apto_celiaco = formData.get("celiacFriendly") ? true : false
+    const apto_lactosa = formData.get("lactoseFree") ? true : false
+
     fetch("http://localhost:5000/recetas",{
         method: "POST",
         headers: {
@@ -36,6 +42,8 @@ function create_recipe(event){
             video: video,
             ingredientes: ingredientes,
             receta: receta,
+            tipo_receta: tipo_receta,
+            dificultad: dificultad,
             calorias: calorias,
             tiempo_preparacion: tiempo_preparacion,
             alto_proteinas: alto_proteinas,
