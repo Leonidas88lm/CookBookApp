501a770dfe365729117f53e05e282128de0871bc
Gonzaa <gweber@fi.uba.ar>
2024-06-28 23:26:37 -0300

    Se agrego las restricciones de registro, y correccion de redirecciones

--- frontend/sesion/registro/RegistroUsuario.js
+++ frontend/sesion/registro/RegistroUsuario.js
@@ -2,44 +2,43 @@ const MIN_CARACTERES = 4
 const MAX_CARACTERES_CONTRASEÑA = 16
 const MAX_CARACTERES_NOMBRE = 12
 
-const parametros_URL = new URLSearchParams(window.location.search);
-const redireccion = parametros_URL.get("url");
+const parametros_URL = new URLSearchParams(window.location.search)
+const redireccion = parametros_URL.get("url")
 
 function handle_response(data) {
   if (data.exito) {
-    if (redireccion === "registro-crear-receta") {
-      window.location.href = `/sesion/inicio/SesionUser.html?url=crear-receta`
-    }
-    else if (redireccion === "registro-mis-recetas") {
-      window.location.href = `/sesion/inicio/SesionUser.html?url=mis-recetas`
-    } 
-  } else {
-    console.error("Respuesta inesperada del servidor:", data);
+    window.location.href = `/sesion/inicio/InicioUsuario.html?url=${redireccion}`
+  }
+  else {
+    mensaje = "nombre de usuario ya en uso"
+    MensajeDatosInvalidos(mensaje)
   }
 }
 
 function crear_usuario(event){
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const nombre = formData.get("nombre");
-  const contraseña = formData.get("contraseña_registro");
-  const contraseña_confirmacion = formData.get("contraseña_registro_confirmacion");
+  event.preventDefault()
+  const formData = new FormData(event.target)
+  const nombre = formData.get("nombre")
+  const contraseña = formData.get("contraseña_registro")
+  const contraseña_confirmacion = formData.get("contraseña_registro_confirmacion")
+  alert(contraseña != contraseña_confirmacion)
 
-    if (!ValidarContraseña(contraseña)) {
-        let mensaje = "La contraseña debe tener entre 4 y 16 caracteres, incluir al menos una letra mayúscula, una letra minúscula, un número y un símbolo."
-        MensajeDatosInvalidos(mensaje)
-        return;
-    } 
-    else if (!validarNombreUsuario(nombre)) {
-        let mensaje = "El nombre debe tener entre 4 y 16 caracteres."
-        MensajeDatosInvalidos(mensaje)
-        return;
-    }
-    else if (contraseña != confirmarContraseña) {
-        alert("Las contraseñas no coinciden, vuelva a intentarlo.")
-        return
-    }
-    
+  if (!ValidarContraseña(contraseña)) {
+    let mensaje = "La contraseña debe tener entre 4 y 16 caracteres, incluir al menos una letra mayúscula, una letra minúscula, un número y un símbolo."
+    MensajeDatosInvalidos(mensaje)
+    return
+  } 
+  if (!ValidarNombreUsuario(nombre)) {
+    let mensaje = "El nombre debe tener entre 4 y 12 caracteres."
+    MensajeDatosInvalidos(mensaje)
+    return
+  }
+  if (contraseña != contraseña_confirmacion) {
+      let mensaje = "Las contraseñas no coinciden, vuelva a intentarlo."
+      MensajeDatosInvalidos(mensaje)
+      return
+  }
+  
   fetch("http://localhost:5000/usuarios/crear", {
     method:"POST",
     headers: {
@@ -57,30 +56,29 @@ function crear_usuario(event){
 
 
 
-  function validarContraseña(contraseña) {
-    const longitudValida = contraseña.length >= MIN_CARACTERES && contraseña.length <= MAX_CARACTERES_CONTRASEÑA;
-    const tieneMayuscula = /[A-Z]/.test(contraseña);
-    const tieneMinuscula = /[a-z]/.test(contraseña);
-    const tieneNumero = /\d/.test(contraseña);
-    const tieneSimbolo = /[!@#$%^&*(),.?":{}|<>]/.test(contraseña);
+  function ValidarContraseña(contraseña) {
+    const longitudValida = contraseña.length >= MIN_CARACTERES && contraseña.length <= MAX_CARACTERES_CONTRASEÑA
+    const tieneMayuscula = /[A-Z]/.test(contraseña)
+    const tieneMinuscula = /[a-z]/.test(contraseña)
+    const tieneNumero = /\d/.test(contraseña)
+    const tieneSimbolo = /[!@#$%^&*(),.?":{}|<>]/.test(contraseña)
 
-    return longitudValida && tieneMayuscula && tieneMinuscula && tieneNumero && tieneSimbolo;
+    return longitudValida && tieneMayuscula && tieneMinuscula && tieneNumero && tieneSimbolo
 }
 
 function ValidarNombreUsuario(nombre) {
-  const longitudValida = nombre.length >= MIN_CARACTERES && contraseña.length <= MAX_CARACTERES_NOMBRE;
-  return longitudValida;
+  const longitudValida = nombre.length >= MIN_CARACTERES && nombre.length <= MAX_CARACTERES_NOMBRE
+  return longitudValida
 }
 
 function MensajeDatosInvalidos(mensaje) {
-  let form = document.getElementById("form-registro")
-  if (form.querySelector("span") == null) {
-      let aviso = document.createElement("span")
-      aviso.setAttribute("id", "aviso")
-      form.append(aviso)
-  } 
-  aviso = document.getElementById("aviso")
+  let aviso = document.getElementById("aviso")
+  let input_contraseña = document.getElementById("contraseña_registro")
+  let input_confirmar_contra = document.getElementById("contraseña_registro_confirmacion")
+  aviso.innerHTML = ''
   aviso.innerText = "*" + mensaje.toLowerCase()
+  input_contraseña.value = ''
+  input_confirmar_contra.value = ''
 }
 
 function redireccion_inicio_sesion() {
